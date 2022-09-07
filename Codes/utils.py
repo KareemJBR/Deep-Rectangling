e3ff3ba86a95577db91fc5e9fc6b2bbc900f03d4
Malik Egbarya <megbarya@salesforce.com>
2022-09-08 02:51:52 +0300

    fixing img baths && generating random augmentation and plotting the resultx1

--- Codes/utils.py
+++ Codes/utils.py
@@ -1,12 +1,9 @@
 import tensorflow as tf
 import numpy as np
 from collections import OrderedDict
-import sys
 import os
 import glob
 import cv2
-import pyttsx3
-import engine
 
 rng = np.random.RandomState(2017)
 
@@ -25,37 +22,16 @@ class DataLoader(object):
             while True:
                 data_clip = []
                 frame_id = rng.randint(0, length - 1)
-                # inputs
-
-                input_img = np_load_frame(data_info_list[1]['frame'][frame_id], 384, 512)
-                mask_img = np_load_frame(data_info_list[2]['frame'][frame_id], 384, 512)
-                gt_img = np_load_frame(data_info_list[0]['frame'][frame_id], 384, 512)
-
-                data_clip.append(input_img)
-                data_clip.append(mask_img)
-                data_clip.append(gt_img)
-                data_clip = np.concatenate(data_clip, axis=2)
-
-                yield data_clip
-
-                # creating augmentations
-
-                data_clip = []
-
-                flipped_input = engine.flip_image(input_img)
-                flipped_mask = engine.flip_image(mask_img)
-                flipped_gt = engine.flip_image(gt_img)
-
-                data_clip.append(flipped_input)
-                data_clip.append(flipped_mask)
-                data_clip.append(flipped_gt)
+                #######inputs
+                data_clip.append(np_load_frame(data_info_list[1]['frame'][frame_id], 384, 512))
+                data_clip.append(np_load_frame(data_info_list[2]['frame'][frame_id], 384, 512))
+                data_clip.append(np_load_frame(data_info_list[0]['frame'][frame_id], 384, 512))
                 data_clip = np.concatenate(data_clip, axis=2)
 
                 yield data_clip
 
         dataset = tf.data.Dataset.from_generator(generator=data_clip_generator, output_types=tf.float32,
                                                  output_shapes=[384, 512, 9])
-
         print('generator dataset, {}'.format(dataset))
         dataset = dataset.prefetch(buffer_size=128)
         dataset = dataset.shuffle(buffer_size=128).batch(batch_size)
@@ -70,12 +46,7 @@ class DataLoader(object):
     def setup(self):
         datas = glob.glob(os.path.join(self.dir, '*'))
         for data in sorted(datas):
-
-            if sys.platform[:3] == 'win':
-                data_name = data.split('\\')[-1]
-            else:
-                data_name = data.split('/')[-1]
-
+            data_name = data.split('/')[-1]
             if data_name == 'gt' or data_name == 'input' or data_name == 'mask':
                 self.datas[data_name] = {}
                 self.datas[data_name]['path'] = data
@@ -98,12 +69,10 @@ class DataLoader(object):
 
 def np_load_frame(filename, resize_height, resize_width):
     image_decoded = cv2.imread(filename)
-
-    if resize_height is not None:
+    if resize_height != None:
         image_resized = cv2.resize(image_decoded, (resize_width, resize_height))
     else:
         image_resized = image_decoded
-
     image_resized = image_resized.astype(dtype=np.float32)
     image_resized = (image_resized / 127.5) - 1.0
     return image_resized
@@ -122,3 +91,7 @@ def save(saver, sess, logdir, step):
         os.makedirs(logdir)
     saver.save(sess, checkpoint_path, global_step=step)
     print('The checkpoint has been created.')
+
+
+
+
